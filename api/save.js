9a1c4b6066fbcfe7575af4dcea09799a6cb7bf53
Adibrill1 <opolit@gmail.com>
2026-04-01 15:36:02 +0300

    Fix image URLs to use direct /images/ static path

--- api/save.js
+++ api/save.js
@@ -67,7 +67,7 @@ module.exports = async function handler(req, res) {
       var putRes   = await ghRequest(token, 'PUT', imgPath, putBody);
 
       if (putRes.status === 200 || putRes.status === 201) {
-        newState.customImgs[key] = '/api/img?p=' + encodeURIComponent(imgPath);
+        newState.customImgs[key] = '/' + imgPath;
       }
     }
 
